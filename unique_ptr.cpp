ca39e31cd491433b7315a22fd59617381b3dded8
hanjing <hanjing2020@foxmail.com>
2023-11-07 22:50:00 +0800

    update unique_ptr

--- unique_ptr.cpp
+++ unique_ptr.cpp
@@ -1,12 +1,10 @@
 #include <cassert>
 #include <iostream>
-#include <fmt/core.h>
-#include <fmt/ranges.h>
-#include <fmt/format.h>
-#include <fmt/os.h>
 #include <vector>
 #include <array>
 #include <fstream>
+#include <utility>
+#include <concepts>
 using namespace std;
 
 struct B
@@ -30,9 +28,8 @@ void close_file(FILE* fp)
 }
 
 template<class T>
-class Deleter
+struct DefaultDeleter
 {
-public:
     void operator() (T* p) const
     {
         delete p;
@@ -40,54 +37,82 @@ public:
 
 };
 
-template<class T, class Deleter=Deleter<T>>
+template<>
+struct DefaultDeleter<FILE> {
+    void operator()(FILE* p) const {
+        fclose(p);
+    }
+};
+
+
+template<class T, class Deleter=DefaultDeleter<T>>
 class Unique_ptr
 {
 private:
     T* m_p;
-    Deleter m_deleter;
 public:
     template<class U, class E>
     friend class Unique_ptr;
-    Unique_ptr(T* p) {
+    Unique_ptr(std::nullptr_t p = nullptr) {
         m_p = p;
     }
-    Unique_ptr(T* p, Deleter deleter) {
+    explicit Unique_ptr(T* p) {
         m_p = p;
-        m_deleter = deleter;
     }
+
+    Unique_ptr(Unique_ptr const &that) = delete;
+    Unique_ptr& operator=(Unique_ptr const& that) = delete;
+
     Unique_ptr(Unique_ptr&& u) {
         cout << __PRETTY_FUNCTION__ << endl;
         m_p = u.m_p;
         u.m_p = nullptr;
     }
-    template<class U, class E>
+    template<class U, class E> requires (std::convertible_to<U *, T *>)
     Unique_ptr(Unique_ptr<U, E>&& u) {
         cout << __PRETTY_FUNCTION__ << endl;
-        m_p = u.m_p;
-        u.m_p = nullptr;
-        // m_deleter = u.m_deleter;
+        m_p = std::exchange(u.m_p, nullptr);
     }
+
+    Unique_ptr& operator=(Unique_ptr&& that) {
+        if (this != &that) {
+            if (m_p)
+                Deleter{}(m_p);
+            m_p = exchange(that.m_p, nullptr);
+        }
+        return *this;
+    }
+
+    explicit operator bool() const {
+        return bool(m_p);
+    }
+
     T* get() const {
         return m_p;
     }
-    Deleter get_deleter() const {
-        return m_deleter;
+    
+    T* release() {
+        return exchange(m_p, nullptr);
     }
-    operator bool() const {
-        return bool(m_p);
+    
+    T& operator*() const {
+        return *m_p;
     }
+
     T* operator ->() const {
         return m_p;
     }
+
     ~Unique_ptr() {
-        m_deleter(m_p);
+        if (m_p) {
+            Deleter{}(m_p);
+        }
     }
 };
 
-template<class T, class Deleter=Deleter<T>>
-Unique_ptr<T, Deleter> make_Unique() {
-    return Unique_ptr<T, Deleter>(new T);
+template<class T, class ...Args>
+Unique_ptr<T> make_Unique(Args&& ...args) {
+    return Unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 
@@ -96,6 +121,38 @@ Unique_ptr<D> pass_through(Unique_ptr<D> p)
     p->bar();
     return p;
 }
+
+struct MyClass {
+    int a, b, c;
+};
+
+struct Animal {
+    virtual void speak() = 0;
+    virtual ~Animal() = default;
+};
+
+struct Dog : Animal {
+    int age;
+
+    Dog(int age_) : age(age_) {
+    }
+
+    virtual void speak() {
+        printf("Bark! I'm %d Year Old!\n", age);
+    }
+};
+
+struct Cat : Animal {
+    int &age;
+
+    Cat(int &age_) : age(age_) {
+    }
+
+    virtual void speak() {
+        printf("Meow! I'm %d Year Old!\n", age);
+    }
+};
+
 int main()
 {
     cout << "1) Unique ownership sematics demo\n";
@@ -114,25 +171,35 @@ int main()
     cout << "\n" "3) Custom deleter demo\n";
     ofstream("demo.txt") << "x";
     {
-        using Unique_file_t = Unique_ptr<FILE, decltype(&close_file)>;
-        Unique_file_t fp(fopen("demo.txt", "r"), &close_file);
+        Unique_ptr<FILE> fp(fopen("demo.txt", "r"));
         if (fp)
             cout << char(fgetc(fp.get())) << "\n";
     }
 
-    cout << "\n" "4) Custom lambda-expression deleter and exception safety demo\n";
-    try
-    {
-        Unique_ptr<D, void(*)(D*)> p(new D, [](D* ptr) 
-        {
-            cout << "destroying from a custom deleter...\n";
-            delete ptr;
-        });
-        throw runtime_error("");
+    // cout << "\n" "4) Custom lambda-expression deleter and exception safety demo\n";
+    // try
+    // {
+    //     Unique_ptr<D, void(*)(D*)> p(new D, [](D* ptr) 
+    //     {
+    //         cout << "destroying from a custom deleter...\n";
+    //         delete ptr;
+    //     });
+    //     throw runtime_error("");
+    // }
+    // catch (const std::exception&)
+    // {
+    //     cout << "Caught exception\n";
+    // }
+    std::vector<Unique_ptr<Animal>> zoo;
+    int age = 3;
+    zoo.push_back(make_Unique<Cat>(age));
+    zoo.push_back(make_Unique<Dog>(age));
+    for (auto const &a: zoo) {
+        a->speak();
     }
-    catch (const std::exception&)
-    {
-        cout << "Caught exception\n";
+    age++;
+    for (auto const &a: zoo) {
+        a->speak();
     }
-
+    
 }
